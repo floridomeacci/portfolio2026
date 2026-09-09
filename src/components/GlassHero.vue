c3b2a86b90bfda3fa3b3d210808bb96ddf885d76
Florido Meacci <floridomeacci@Floridos-MacBook-Pro.local>
2026-09-09 16:36:18 +0200

    Bundle draco decoder locally to satisfy CSP

--- src/components/GlassHero.vue
+++ src/components/GlassHero.vue
@@ -78,7 +78,7 @@ onMounted(() => {
   }
 
   const draco = new DRACOLoader()
-  draco.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/')
+  draco.setDecoderPath('/draco/')
   const loader = new GLTFLoader()
   loader.setDRACOLoader(draco)
   loader.load('/models/hero.glb', onModelLoaded, undefined, () => {})
